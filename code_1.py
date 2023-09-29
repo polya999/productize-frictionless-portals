db6f229af16ea80a094777dd5273768401241bc0
Polina <p9991820@gmail.com>
2023-09-29 10:52:24 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,56 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+function main() {
+
+    data.forEach(item => {
+
+    });
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+}
+
+    }
+main();
+}
+    const data = [];
+    return data;    data = generate_random_data()
+
+
+import random
+    for item in data:
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    main()
+
+
+def main():
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+import random
+if __name__ == "__main__":
+def generate_random_data():
+
+
+    main()
+    return data
+def main():
+    data = generate_random_data()
+    for item in data:    main()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+    data = generate_random_data()
+
+
+def main():
+    return data
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
