742355192800c6c08c10829f0119571583c5b9d7
Polina <p9991820@gmail.com>
2023-10-01 23:17:43 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,26 +1,56 @@
-    return data
-import random
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
+    const data = generateRandomData();
+main();
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+}
 
-    for item in data:
+function main() {
+
+    return data;
+    const data = [];
+    }
+
+        console.log("Random Number: " + item);
+    });
+}
+function generateRandomData() {
+
+
+    return data
 if __name__ == "__main__":
+    for item in data:
+    data = generate_random_data()
+
+import random
+def main():
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def generate_random_data():import random
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+
 
 def generate_random_data():
 
-
-        print(f"Random Number: {item}")
+    main()
+    for item in data:
 def main():
-    main()            System.out.println("Random Number: " + item);
-    }
-}
-        for (int i = 0; i < 10; i++) {
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
+def main():
+    return data
 
-        }
+import random
 
-        int[] data = new int[10];
-        }
-        for (int item : data) {
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-            data[i] = (int) (Math.random() * 100) + 1;
+if __name__ == "__main__":
+    for item in data:
+
+    main()
+        print(f"Random Number: {item}")
